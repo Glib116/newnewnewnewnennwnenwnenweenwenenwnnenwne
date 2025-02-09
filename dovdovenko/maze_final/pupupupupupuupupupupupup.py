34dfd1d2333c86e528f3136c54b142dd396850b5
Glib116 <glebtomas99@gmail.com>
2025-02-09 12:53:39 +0200

    Update pupupupupupuupupupupupup.py

--- dovdovenko/maze_final/pupupupupupuupupupupupup.py
+++ dovdovenko/maze_final/pupupupupupuupupupupupup.py
@@ -10,34 +10,66 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-        
-        
         def reset(self):
             window.blit(self.image, (self.rect.y, self.rect.x))
+class enemy(GameSprite):
+    direction = "left"
+    
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = "right"
+        if self.rect.x >=win_width - 85:
+            self.direction = "left"
             
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+        
+            
+
+
+class Player(GameSprite):
+        def update (self):
+            keys=keys.get_pressed()
+            if keys(K_LEFT) and self.rect.x > 5:
+                self.rect.x = self.speed   
+            if keys(K_RIGHT) and self.rect.x - win_width - 80:
+                self.rect.x = self.speed  
+            if keys(K_UP) and self.rect.x > 5:
+                self.rect.x = self.speed  
+            if keys(K_DOWN) and self.rect.x - win_height - 80:
+                self.rect.x = self.speed        
 win_width = 700
 win_height = 500
+
+
+finish = False
+game = True
+clock = time.Clock()
+FPS = 60
+player = GameSprite('hero.png', 5, win_height - 80, 4)
+monster = GameSprite('cyborg.png', 5, win_width - 80, 280, 2)
+final = GameSprite('treasure.png', 5, win_width - 120, win_height - 80, 0)
+mixer.init()
+mixer.music.load('jungles.ogg')
+mixer .music.play()
+
+
 
 window = display.set_mode(win_width, win_height)
 display.set_caption('Maze')
 background = transform.scale(image.load('background.jpg'),(win_height, win_width))
 
-player = GameSprite('hero.png', 5, win_height - 80, 4)
-monster = GameSprite('cyborg.png', 5, win_width - 80, 280, 2)
-final = GameSprite('treasure.png', 5, win_width - 120, win_height - 80, 0)
-game = True
-clock = time.Clock()
-FPS = 60
-mixer.init()
-mixer.music.load('jungles.ogg')
-mixer .music.play()
 while game:
     for e in event.get():
-        if e.type == QUET:
+        if e.type == QUIT:
             game = False
-            
-    window.blit(background, (0,0))
-    player.reset()
-    monster.reset()
-    display.update()
-    clock.tick(FPS)
+    if finish != True:
+        window.blit(background(0, 0))
+        player.update()
+        monster.update()
+        
+        player.reset()
+        monster.reset()
+        final.reset()
