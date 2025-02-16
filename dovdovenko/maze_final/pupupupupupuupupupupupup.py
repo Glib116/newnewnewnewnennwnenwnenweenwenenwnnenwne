7ea40d863ab736b746943d25fa05d837423c5f28
Glib116 <glebtomas99@gmail.com>
2025-02-16 13:15:07 +0200

    Update pupupupupupuupupupupupup.py

--- dovdovenko/maze_final/pupupupupupuupupupupupup.py
+++ dovdovenko/maze_final/pupupupupupuupupupupupup.py
@@ -44,13 +44,54 @@ win_width = 700
 win_height = 500
 
 
+class Wall(sprite.Sprite):
+    def __init__(self,color_1,color_2,color_3,wall_height,wall_x, wall_y, wall_width,):
+        super().__init__()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = wall_width
+        self.height = wall_height
+
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
+
+        self.rect = self.image.get_rect()
+        self.rect_x = wall_x
+        self.rect_y = wall_y
+
+
+    def draw_wall(self):
+        window.blit(self.image, (self.rect_x,self.rect_y))
+
+
+win_width = 700
+win_height = 500
+
+
+w1 = Wall(154,205,50,100,20,450,10)
+w2 = Wall(154,205,50,100,480,350,10)
+w3 = Wall(154,205,50,100,20,10,380)
+
+player = GameSprite('hero.png', 5, win_height - 80, 4)
+monster = GameSprite('cyborg.png', 5, win_width - 80, 280, 2)
+final = GameSprite('treasure.png', 5, win_width - 120, win_height - 80, 0)
+
 finish = False
 game = True
 clock = time.Clock()
 FPS = 60
-player = GameSprite('hero.png', 5, win_height - 80, 4)
-monster = GameSprite('cyborg.png', 5, win_width - 80, 280, 2)
-final = GameSprite('treasure.png', 5, win_width - 120, win_height - 80, 0)
+
+
+font.init()
+font = font.Font(None, 70)
+win = font.render ("YOU WIN!",True,(255,215,0))
+win = font.render ("YOU LOSE!",True,(180,0,0))
+
+
+
+
+
 mixer.init()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
@@ -74,5 +115,26 @@ while game:
         monster.reset()
         final.reset()
 
-    display.update()
-    clock.tick(FPS)
+        w1.draw_wall()
+        w1.draw_wall()
+        w1.draw_wall()
+
+
+
+if sprite.collide_rect(player,monster) or sprite.collide_rect:
+    (player, w1) or sprite.collide_rect(player, w2) or sprite
+    collide_rect(player, w3)
+    finish = True
+    window.blit(lose, (200,200))
+    kick.play()
+
+
+if sprite.collide_rect(player,final):
+    finish = True 
+    window.blit(win,(200,200))
+    money.play()
+
+
+
+display.update()
+clock.tick(FPS)
